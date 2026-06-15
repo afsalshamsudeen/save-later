5148efb6abacdf41c6fb28f884333febe19f737c
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2026-06-15 16:02:50 +0000

    feat(auth): duplicate email and username check added

--- src/services/auth_services.py
+++ src/services/auth_services.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from src.models.user import User
@@ -7,6 +8,26 @@ from src.core.security import hash_password
 
 def create_user(db: Session, user: UserRegister):
     hashed_password = hash_password(user.password)
+
+    existing_email = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_email:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+    
+    existing_username = db.query(User).filter(
+        User.username == user.username
+    ).first()
+
+    if existing_username:
+        raise HTTPException(
+            status_code=400,
+            detail="Username already taken"
+        )
 
     new_user = User(
         username = user.username,
